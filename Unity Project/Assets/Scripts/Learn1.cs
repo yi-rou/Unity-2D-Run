9f6436fd1a92b9ae3df1bea1078a721aa784fed3
yi-rou <57440062+yi-rou@users.noreply.github.com>
2020-07-04 21:00:15 +0800

    欄位方法

---
+++ Unity Project/Assets/Scripts/Learn1.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Learn1 : MonoBehaviour
+{
+    private void Start()
+    {
+        print(Mathf.Epsilon);
+        print(Mathf.Rad2Deg);
+        print(Random.value);
+        print(Mathf.Abs(-10.5f)); //取得絕對值
+        
+    }
+
+    private void Update()
+    {
+        print(Random.Range(100, 501));
+    }
+}
---
+++ Unity Project/Assets/Scripts/Player.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    // 單行註解
+
+    /*
+     * 多行註解
+     * 多行註解
+     */
+
+    #region 欄位區域
+
+    /* 
+     * 命名規則
+     * 1. 用有意義的名稱
+     * 2. 不要使用數字開頭
+     * 3.不要使用特殊符號 @#/* 空格
+     * 4. 可以使用中文(建議不要)
+     */
+
+    // Header 標題
+    // Tooltip 提示
+    // Range 範圍
+    [Header("速度"),Tooltip("角色速度"),Range(50, 500)]
+    public int speed = 59;
+    [Header("血量"),Tooltip("角色血量"),Range(500, 1500)]
+    public float hp = 799.99f;
+    public int coin;
+    [Header("跳躍高度"),Range(500, 1000)]
+    public int height = 659;
+    [Header("音效區域")]
+    public AudioClip soundJump;
+    public AudioClip soundSlide;
+    public AudioClip soundHit;
+    [Header("是否死亡")]
+    public bool dead;
+
+    #endregion
+
+    #region  方法區域
+
+    /// <summary>
+    /// 跳躍音效.跳躍動畫
+    /// </summary>
+    private void Jump()
+    {
+        print("跳躍");
+    }
+
+    /// <summary>
+    /// 滑行動畫.滑行音效.縮小碰撞範圍
+    /// </summary>
+    private void Slide()
+    {
+
+    }
+
+    /// <summary>
+    /// 碰到障礙物時會扣血
+    /// </summary>
+    private void Hit()
+    {
+
+    }
+
+    /// <summary>
+    /// 吃到金幣會增加數量.更新介面.吃金幣音效
+    /// </summary>
+    private void EatCoin()
+    {
+
+    }
+
+    /// <summary>
+    /// 死亡動畫.遊戲結束
+    /// </summary>
+    private void Dead()
+    {
+
+    }
+
+    #endregion
+
+    #region 事件區域
+
+
+    private void Start()
+    {
+        
+    }
+
+    //一秒執行60次
+    //監控玩家鍵盤.滑鼠與觸控
+    private void Update()
+    {
+         
+    }
+    #endregion
+}
