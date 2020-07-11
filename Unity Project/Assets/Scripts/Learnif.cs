10a562757bd83da3f2326f2bc61788c5e7d2361d
yi-rou <57440062+yi-rou@users.noreply.github.com>
2020-07-11 20:58:33 +0800

    移動

---
+++ Unity Project/Assets/Scripts/Learnif.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Learnif : MonoBehaviour
+{
+    public bool test;
+
+    private void Update()
+    {
+        if (test)
+        {
+            print("cc");
+        }
+        else
+        {
+            print("kk");
+        }
+    }
+}
--- Unity Project/Assets/Scripts/Player.cs
+++ Unity Project/Assets/Scripts/Player.cs
@@ -23,13 +23,13 @@ public class Player : MonoBehaviour
     // Header 標題
     // Tooltip 提示
     // Range 範圍
-    [Header("速度"),Tooltip("角色速度"),Range(50, 500)]
-    public int speed = 59;
-    [Header("血量"),Tooltip("角色血量"),Range(500, 1500)]
+    [Header("速度"), Tooltip("角色速度"), Range(1, 50)]
+    public int speed = 8;
+    [Header("血量"), Tooltip("角色血量"), Range(500, 1500)]
     public float hp = 799.99f;
     public int coin;
-    [Header("跳躍高度"),Range(500, 1000)]
-    public int height = 659;
+    [Header("跳躍高度"), Range(100, 1000)]
+    public int height = 500;
     [Header("音效區域")]
     public AudioClip soundJump;
     public AudioClip soundSlide;
@@ -38,6 +38,12 @@ public class Player : MonoBehaviour
     public bool dead;
     [Header("動畫控制器")]
     public Animator ani;
+
+    public CapsuleCollider2D cc2D;
+
+    public Rigidbody2D rig;
+
+    public bool isGround;
     #endregion
 
     #region  方法區域
@@ -45,10 +51,29 @@ public class Player : MonoBehaviour
     /// <summary>
     /// 跳躍音效.跳躍動畫
     /// </summary>
+
+    private void Move()
+    {
+        //   剛體.加速度.大小
+        if (rig.velocity.magnitude < 10)
+        {
+            rig.AddForce(new Vector2(speed, 0));
+        }
+    }
+
     private void Jump()
     {
         bool code = Input.GetKey(KeyCode.J);
-        ani.SetBool("跳躍開關", code);
+        ani.SetBool("跳躍開關", !isGround);
+        if (isGround)
+        {
+            if (code)
+            {
+                isGround = false;
+                rig.AddForce(new Vector2(0, height));
+            }
+
+        }
     }
 
     /// <summary>
@@ -58,6 +83,16 @@ public class Player : MonoBehaviour
     {
         bool key = Input.GetKey(KeyCode.S);
         ani.SetBool("滑行開關", key);
+        if (key)
+        {
+            cc2D.offset = new Vector2(-1.3f, -0.7f);
+            cc2D.size = new Vector2(1.7f, 2);
+        }
+        else
+        {
+            cc2D.offset = new Vector2(-1.3f, 0.05f);
+            cc2D.size = new Vector2(1.7f, 3.8f);
+        }
     }
 
     /// <summary>
@@ -91,15 +126,31 @@ public class Player : MonoBehaviour
 
     private void Start()
     {
-        
+
     }
 
     //一秒執行60次
     //監控玩家鍵盤.滑鼠與觸控
     private void Update()
     {
-        Jump();
         Slide();
     }
+
+    /// <summary>
+    /// 如果有剛體的方法 建議使用
+    /// </summary>
+    private void FixedUpdate()
+    {        
+        Move();
+        Jump();
+    }
     #endregion
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "地板")
+        {
+            isGround = true;
+        }
+    }
 }
